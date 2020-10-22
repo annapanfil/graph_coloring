e429f7479e54e8e2dd010c2556ad6ff91653dfc6
panka134 <134panka@gmail.com>
2020-10-22 19:38:10 +0200

    add class Graf

--- main.py
+++ main.py
@@ -1,55 +1,64 @@
 import random
 
-def printList(list):
-    # print("\nLIST")
-    for row in range(len(list)):
-        print(row, ".", end=" ")
-        for col in range(len(list[row])):
-            print(list[row][col], end=" ")
+class Graf():
+    def __init__(self, rozmiar = random.randint(3,7)):
+        self.rozmiar = rozmiar      # ile wierzchołków w grafie
+        self.kolory = 1             # ile kolorów jest użytych jak na razie
+        self.sasiedzi = []          # każda podlista to zbiór sąsiadów danego wierzchołka
+        self.kolorowanie = [0 for _ in range(rozmiar)]   # kolory numerujemy od 1 w górę, 0 na pozycji kolorów oznacza, że wierzchołek jest jeszcze nie pokolorowany
+
+    def generuj_krawedzie(self):
+        for i in range(self.rozmiar):
+            self.sasiedzi.append([])
+        for i in range(self.rozmiar):
+            for j in range(i+1, self.rozmiar):
+                czy_krawedz = random.randint(0,1)
+                if czy_krawedz == 1:
+                    self.sasiedzi[i].append(j)
+                    self.sasiedzi[j].append(i)
+
+    def pokaz_liste_incydencji(self):
+        print("LISTA INCYDENCJI")
+        for row in range(len(self.sasiedzi)):
+            print(row, ".", end=" ")
+            for col in range(len(self.sasiedzi[row])):
+                print(self.sasiedzi[row][col], end=" ")
+            print()
         print()
 
-rozmiar = random.randint(3,7)         # ile wierzchołków w grafie
-kolory = 1                            # mówi ile kolorów jest użytych jak na razie
-sasiedzi=[]                           # każda podlista to zbiór sąsiadów danego wierzchołka
-for i in range(rozmiar):
-    sasiedzi.append([])
-for i in range(rozmiar):
-    for j in range(i+1, rozmiar):
-        czy_krawedz = random.randint(0,1)
-        if czy_krawedz == 1: # and j != i:
-            sasiedzi[i].append(j)
-            sasiedzi[j].append(i)
+    def pokaz_kolorowanie(self):
+        print("KOLOROWANIE")
+        for i in range(self.rozmiar):
+            print(i, ".", self.kolorowanie[i])
 
+        print("Użyto", self.kolory, "koloru/ów.\n")
 
-kolorowanie=[]
-for i in range(rozmiar):
-    kolorowanie.append([i, 0])        # wierzchołki od numeruję 0, a kolory od 1 w górę, 0 na pozycji kolorów oznacza, że wierzchołek jest jeszcze nie pokolorowany
-                                      # można zapisać to w postaci prostej listy bez zagnieżdżania podlist
+    def koloruj_wierzcholek(self, v: int):
+        for kolor in range(1, self.kolory+1):
+            uzyty = False
+            for sasiad in self.sasiedzi[v]:
+                if self.kolorowanie[sasiad] == kolor:
+                    uzyty = True
+                    break
+            if uzyty == False:
+                self.kolorowanie[v] = kolor
+                return
 
-def koloruj(v):                       # v to numer wierzchołka
-    global kolory
-    for kolor in range(1, kolory+1):
-        uzyty = False
-        for sasiad in sasiedzi[v]:
-            if kolorowanie[sasiad][1] == kolor:
-                uzyty = True
-                break
-        if uzyty == False:
-            kolorowanie[v][1] = kolor
-            return
-    # if kolorowanie[v][1] == 0: # jak zrobimy returny, to nie powinien tu dojść
-    kolory += 1
-    kolorowanie[v][1] = kolory
-    return
+        self.kolory += 1
+        self.kolorowanie[v] = self.kolory
+        return
 
+    def koloruj_graf(self):
+        for v in range(self.rozmiar):
+            self.koloruj_wierzcholek(v)
 
 
 def main():
-    print("Wylosowany graf ma ", rozmiar, "wierzchołków.")
-    print("Lista incydencji:")
-    printList(sasiedzi)
-    for v in range(rozmiar):
-        koloruj(v)
-    print("Kolorowanie:", kolorowanie)
-    print("Użyto", kolory, "kolory/ów.")
-main()
+    g = Graf()
+    g.generuj_krawedzie()
+    g.pokaz_liste_incydencji()
+    g.koloruj_graf()
+    g.pokaz_kolorowanie()
+
+if __name__ == '__main__':
+    main()
